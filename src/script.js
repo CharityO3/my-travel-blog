0d0d4e67ccf7fa43cb6f64dbb6a16c7c551df134
Charity Orhoridiohwo <79048698+CharityO3@users.noreply.github.com>
2025-04-18 15:31:59 +0200

    Inject nav test file to project
    - Rename test file to nav-test

--- src/script.js
+++ src/script.js
@@ -27,3 +27,9 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 });
+
+let menuToggle = document.querySelector('.menu-toggle');
+let header = document.querySelector('header');
+menuToggle.onclick = function(){
+  header.classList.toggle('active');
+}
